ede648a07861fd0fc97818afae673cb5a3eeb9cf
Chandi1990 <chandima.erc@gmail.com>
2024-06-23 02:10:35 +0530

    Add project files.

---
+++ AiInsole.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+namespace AiInsole.Api.Data;
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+        base(options)
+    { }
+}
